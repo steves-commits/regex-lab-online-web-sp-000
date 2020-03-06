10364ec2dacff8da066c69ffc901547b58edec16
Steven Topazio <sat@northpointprinting.com>
2020-03-06 20:22:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
 def starts_with_a_vowel?(word)
+# (/[aeiou])
 
+if word.match(/\A[aeiou]/i) == NIL
+  return FALSE
+else
+  return TRUE
+end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
+  text.scan(/\bun\w+ing\b/)
 end
 
+
 def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z].*\.$/) == NIL
+  return FALSE
+else
+  return TRUE
+end
 end
 
 def valid_phone_number?(phone)
-
+if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{4})/) == NIL
+  return FALSE
+else
+  return TRUE
+end
 end
